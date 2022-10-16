c4f83a0c3e109a4fd67fe6540339f3eb22b7819d
George <74255152+georgevanuta@users.noreply.github.com>
2022-10-16 16:11:34 +0300

    Add files via upload

---
+++ src/SimplifyPath/Solution.scala
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/simplify-path/
+
+package SimplifyPath
+
+import scala.collection.mutable.Stack
+
+object Solution {
+	def simplifyPath(path: String): Any = {
+		val dirs: List[String] =
+			path
+			.replaceAll("""/+""", "/")
+			.split("/")
+			.filter(!List("", ".").contains(_))
+			.toList
+
+		val simplifiedDirs : Stack[String] = Stack()
+
+		for (dir <- dirs) {
+			dir match {
+				case ".." => if (!simplifiedDirs.isEmpty) simplifiedDirs.pop()
+				case _	  => simplifiedDirs.push(dir)
+			}
+		}
+
+		"/" + simplifiedDirs.reverse.mkString("/")
+	}
+}
+
+
+object Main {
+	def main(args: Array[String]): Unit = {
+		println(Solution.simplifyPath("/hi/iam/../.././start////wtf"))
+	}
+}
