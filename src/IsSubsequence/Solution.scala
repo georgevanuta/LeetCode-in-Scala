e39b3a8400619b36b46c43ff735dbf23ef920525
georgevanuta <georgevanuta01@gmail.com>
2023-02-09 11:53:00 +0200

    Added 392

---
+++ src/IsSubsequence/Solution.scala
@@ -0,0 +1,12 @@
+package IsSubsequence
+
+
+// https://leetcode.com/problems/is-subsequence/description/
+object Solution {
+	def isSubsequence(s: String, t: String): Boolean = {
+		if (s.isEmpty) true
+		else if (t.isEmpty) false
+		else if (s.head == t.head) isSubsequence(s.tail, t.tail)
+		else isSubsequence(s, t.tail)
+	}
+}
