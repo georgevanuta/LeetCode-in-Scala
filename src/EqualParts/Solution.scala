8b2e180c4af4e79ac29464ae893c57b4b12c2cb7
georgevanuta <georgevanuta01@gmail.com>
2023-03-11 16:54:20 +0200

    added 2206

---
+++ src/EqualParts/Solution.scala
@@ -0,0 +1,17 @@
+package EqualParts
+
+import scala.annotation.tailrec
+import scala.collection.immutable.HashMap
+
+object Solution {
+	def divideArray(nums: Array[Int]): Boolean = {
+		@tailrec
+		def occMap(nums: Array[Int], accMap: HashMap[Int, Int] = HashMap()): HashMap[Int, Int] = {
+			if (nums.isEmpty) accMap
+			else occMap(nums, accMap + (nums.head -> (accMap.getOrElse(nums.head, 0) + 1)))
+		}
+
+		occMap(nums)
+		.forall(_._2 % 2 == 0)
+	}
+}
